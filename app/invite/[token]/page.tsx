fe726195a48ae7372a92b33df241d6ac78674713
miraclejester <miraclejester413@gmail.com>
2026-08-21 13:37:45 -0400

    Fix bad import

--- app/invite/[token]/page.tsx
+++ app/invite/[token]/page.tsx
@@ -1,9 +1,9 @@
 ﻿import { JSX } from 'react';
-import { type InviteWithDetails, redeemInvite } from '@/lib/groups/invites';
+import { redeemInvite } from '@/lib/groups/invites';
 import { getCurrentUser, LoggedInUser } from '@/lib/auth';
 import { RedeemButton } from '@/components/RedeemButton';
 import Link from 'next/link';
-import { getInvite } from '@/lib/groups/inviteQueries';
+import {getInvite, InviteWithDetails} from '@/lib/groups/inviteQueries';
 
 type InvitePageProps = {
     params: Promise<{ token: string }>;
